3ad5025261b5e835f664e9ae213c93bcdcff337e
AnconaEsselmann <Axel@AnconaEsselmann.com>
2013-10-23 21:02:01 -0700

    Std has unit tests

--- classes_and_tests/srcTest/StdTest.py
+++ classes_and_tests/srcTest/StdTest.py
@@ -5,10 +5,33 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.abspath(path.join(__file__, "..", "..")))
 
 from src.Std import Std
+from os import path, sep
+
+data = [
+			(["folder"], "folder"),
+			(["folder", "folder2", "folder3"], "folder" + sep + "folder2" + sep + "folder3"),
+			([sep, "folder2", "folder3"], sep + "folder2" + sep + "folder3")
+		]
 
 class StdTest(unittest.TestCase):
 	def test___init__(self):
 		obj = Std()
+
+	def test_dirExplode(self):
+		global data
+		obj = Std()
+
+		for expected, inputValue in data:
+			result = obj.dirExplode(inputValue)
+			self.assertEqual(expected, result)
+
+	def test_dirImplode(self):
+		global data
+		obj = Std()
+
+		for inputValue, expected in data:
+			result = obj.dirImplode(inputValue)
+			self.assertEqual(expected, result)
 
 	def test_getLineIndentAsWhitespace(self):
 		obj = Std()
